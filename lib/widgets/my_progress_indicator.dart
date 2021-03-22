4762f6d1550029406a33c73d4ded0c3ec07be6f4
ssorrychoi <zzaesung17@gmail.com>
2021-03-23 00:19:06 +0900

    add 18 extract Widget
    -18강 extract위젯

---
+++ lib/widgets/my_progress_indicator.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyProgressIndicator extends StatelessWidget {
+  final double containerSize;
+  final double progressSize;
+
+  const MyProgressIndicator(
+      {Key key, this.containerSize = 60, this.progressSize})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: containerSize,
+      height: containerSize,
+      child: Center(
+        child: SizedBox(
+          height: progressSize,
+          width: progressSize,
+          child: Image.asset('assets/images/cloud_loading.gif'),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,15 +1,62 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:insta_clone_coding/constants/common_size.dart';
+import 'package:insta_clone_coding/widgets/my_progress_indicator.dart';
+import 'package:insta_clone_coding/widgets/rounded_avatar.dart';
 
 class Post extends StatelessWidget {
   final int index;
+  Size size;
 
-  Post(this.index);
+  Post(this.index, {Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.accents[index % Colors.accents.length],
-      height: 100,
+    if (size == null) size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        _postHeader(),
+        _postImage(),
+      ],
+    );
+  }
+
+  CachedNetworkImage _postImage() {
+    return CachedNetworkImage(
+      imageUrl: 'https://picsum.photos/id/$index/2000/2000',
+      placeholder: (context, url) {
+        return MyProgressIndicator(
+          containerSize: size.width,
+        );
+      },
+      imageBuilder: (context, imageProvider) {
+        return AspectRatio(
+          aspectRatio: 1,
+          child: Container(
+            decoration: BoxDecoration(
+                image:
+                    DecorationImage(image: imageProvider, fit: BoxFit.cover)),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _postHeader() {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(common_xxs_gap),
+          child: RoundedAvatar(),
+        ),
+        Expanded(child: Text('UserName')),
+        IconButton(
+          icon: Icon(
+            Icons.more_horiz,
+            color: Colors.black87,
+          ),
+        )
+      ],
     );
   }
 }
